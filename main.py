dead3771167a84ace0df26893fd15cc9b808116f
Thedoree22 <jmr466587@gmail.com>
2025-10-14 21:38:57 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import os
+
+# Railway ამ ტოკენს თავისით წაიკითხავს საიდუმლო ველიდან
+BOT_TOKEN = os.environ['BOT_TOKEN']
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+    await bot.load_extension('counting_cog')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(f"Error syncing commands: {e}")
+
+bot.run(BOT_TOKEN)
